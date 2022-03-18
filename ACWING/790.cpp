84671a9b38200accf3a3ac2dcb00dbe3b33002d9
Lickton <lilihuanmao@gmail.com>
2022-03-18 19:56:50 +0800

    rm -rf ./*

--- ACWING/790.cpp
+++ ACWING/790.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     double x;
     cin >> x;
-    
+
     double l = -10000, r = 10000;   //使可以取到的数的范围在给定的范围以内
     while (r - l > 1e-8) {
         double mid = (l + r) / 2;
@@ -14,7 +14,7 @@ int main() {
         else
             l = mid;
     }
-    
+
     printf("%lf", l);
     return 0;
 }
